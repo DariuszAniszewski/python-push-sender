191af1f8743326d2e8f7dcbcd50e80b0aa27ff08
Dariusz Aniszewski <dariusz@aniszewski.eu>
2015-12-04 10:52:55 +0100

    Small clean-up

--- PushSender.py
+++ PushSender.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Dariusz Aniszewski,Polidea'
-import ssl,socket,struct,simplejson
+import json
+import socket
+import ssl
+import struct
+
+__author__ = 'Dariusz Aniszewski, Polidea'
+
 
 class PushSender():
     ssl_connection = None
     notifications = []
     sandbox = False
-    sandbox_host = ( "gateway.sandbox.push.apple.com", 2195 )
-    production_host = ( "gateway.push.apple.com", 2195 )
+    sandbox_host = ("gateway.sandbox.push.apple.com", 2195)
+    production_host = ("gateway.push.apple.com", 2195)
     verbose = True
-    def __init__(self,certificate_file,sandbox=False,verbose=True):
-        self.ssl_connection = ssl.wrap_socket( socket.socket(), certfile = certificate_file )
+
+    def __init__(self, certificate_file, sandbox=False, verbose=True):
+        self.ssl_connection = ssl.wrap_socket(socket.socket(), certfile=certificate_file)
         self.sandbox = sandbox
         self.verbose = verbose
         if self.verbose:
@@ -18,42 +24,44 @@ class PushSender():
             print "Using cert: %s" % certificate_file
             print "Using sandbox: %s" % sandbox
 
-    def _connect(self):
+    def __connect(self):
         if self.sandbox:
             host = self.sandbox_host
         else:
             host = self.production_host
-        self.ssl_connection.connect( host )
+        self.ssl_connection.connect(host)
         if self.verbose:
-            print "Connected to %s at port %s" % (host[0],host[1])
+            print "Connected to %s at port %s" % (host[0], host[1])
 
-    def _disconnect(self):
+    def __disconnect(self):
         self.ssl_connection.close()
         if self.verbose:
             print "Connection closed"
 
-    def _write(self,data):
-        self.ssl_connection.write( data )
+    def __write(self, data):
+        self.ssl_connection.write(data)
 
+    def __cleanup(self):
+        self.notifications = []
 
-    def addNotification(self,payload_dict,device_token):
-        if None in [payload_dict,device_token]:
+    def add_notification(self, payload_dict, device_token):
+        if None in [payload_dict, device_token]:
             raise ValueError("wrong params")
-        self.notifications.append((payload_dict,device_token))
+        self.notifications.append((payload_dict, device_token))
 
-    def push(self):
+    def send_notifications(self):
         if len(self.notifications) == 0:
             if self.verbose:
                 print "Nothing to push"
             return
-        self._connect()
+        self.__connect()
         for notification in self.notifications:
-            payload = simplejson.dumps(notification[0])
+            payload = json.dumps(notification[0])
             if self.verbose:
-                print "Sending %s to %s" % (payload,notification[1])
+                print "Sending %s to %s" % (payload, notification[1])
             token = bytes(notification[1].decode('hex'))
             format = "!BH32sH%ds" % len(payload)
-            push = struct.pack( format, 0, 32, token, len(payload), payload )
-            self._write(push)
-        self._disconnect()
-
+            push = struct.pack(format, 0, 32, token, len(payload), payload)
+            self.__write(push)
+        self.__disconnect()
+        self.__cleanup()
